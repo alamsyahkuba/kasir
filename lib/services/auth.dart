24ceec1dd10ea0924b146e87517ecf7c3bd9e88c
alamsyahkuba <alam.xpplg2@gmail.com>
2025-01-13 09:56:29 +0700

    update autentikasi login

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,6 +1,7 @@
 import 'package:bcrypt/bcrypt.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:aplikasi_kasir/pages/login.dart';
 
 final supabase = Supabase.instance.client;
@@ -9,13 +10,17 @@ Future auth(String email, String password) async {
   try {
     final response = await supabase
         .from('users')
-        .select('password')
+        .select('id, password')
         .eq('email', email)
         .maybeSingle();
 
-    final hashedPassword = response?['password'];
+    final hashedPassword = response!['password'];
+    final userId = response['id'].toString();
 
     if (BCrypt.checkpw(password, hashedPassword)) {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString('user_id', userId);
+
       return true;
     } else {
       return false;
@@ -27,7 +32,7 @@ Future auth(String email, String password) async {
   }
 }
 
-Future<void> logOut(BuildContext context) async {
+Future logOut(BuildContext context) async {
   await supabase.auth.signOut();
   Navigator.pushReplacement(
     context,
