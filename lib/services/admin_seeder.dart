3864cb1b7d02562b01657dd772a7f4c3be08f4fc
alamsyahkuba <alam.xpplg2@gmail.com>
2025-01-13 08:48:42 +0700

    update admin seeder

--- lib/services/admin_seeder.dart
+++ lib/services/admin_seeder.dart
@@ -2,9 +2,6 @@ import 'package:bcrypt/bcrypt.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future seedDataOnce() async {
-  // final supabase = SupabaseClient('https://kgrbanqnlpahtqpmtqza.supabase.co',
-  //     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtncmJhbnFubHBhaHRxcG10cXphIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczNjEyNTY3NiwiZXhwIjoyMDUxNzAxNjc2fQ.J9aUg9_GBs4qK2vtsGQXs2PI0rAo4V9ji0xJb2FSNyI');
-
   final supabase = Supabase.instance.client;
 
   final response = supabase
